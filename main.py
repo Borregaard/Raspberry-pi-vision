5bc8bee63b3332c63de199beea27c9d6319bb580
borregaard <frederik.borregaard@gmail.com>
2023-01-13 12:22:58 +0100

    LED_setup

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+LED_PIN = 14
+
+GPIO.setwarnings(False)
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(LED_PIN, GPIO.OUT)
+GPIO.output(LED_PIN, True)
+sleep(2)
+GPIO.output(LED_PIN, False)
+
+
 print('Hello, world!')
