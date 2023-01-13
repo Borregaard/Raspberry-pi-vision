b7ac4dee4e6f25316d8341e456362a746b584691
borregaard <frederik.borregaard@gmail.com>
2023-01-13 12:25:39 +0100

    for loop LED

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+print('Hello, world!')
+
 LED_PIN = 14
 
 GPIO.setwarnings(False)
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LED_PIN, GPIO.OUT)
-GPIO.output(LED_PIN, True)
-sleep(2)
-GPIO.output(LED_PIN, False)
 
-
-print('Hello, world!')
+for _ in range(20):
+    GPIO.output(LED_PIN, True)
+    sleep(2)
+    GPIO.output(LED_PIN, False)
+    sleep(2)
