7a4002b80b5fee75e412f214f4af04bc9a8a7fd4
borregaard <frederik.borregaard@gmail.com>
2023-01-13 13:25:27 +0100

    atempt 1

--- main.py
+++ main.py
@@ -2,23 +2,25 @@ import RPi.GPIO as GPIO
 from time import sleep
 from picamera import PiCamera
 
-camera = PiCamera()
 
-sleep(2)
-camera.capture("/Pictures/img.jpg")
-print("Done.")
-
-print('Hello, world!')
-
-LED_PIN = 14
-
-GPIO.setwarnings(False)
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(LED_PIN, GPIO.OUT)
-
-for _ in range(20):
-    GPIO.output(LED_PIN, True)
+def CameraModule():
+    camera = PiCamera()
+    camera.resolution = (1024, 768)
+    camera.start_preview()
+    # Camera warm-up time
     sleep(2)
-    GPIO.output(LED_PIN, False)
-    sleep(2)
+    camera.capture('foo.jpg')
+
+def LedBlink():
+    LED_PIN = 14
+
+    GPIO.setwarnings(False)
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LED_PIN, GPIO.OUT)
+
+    for _ in range(20):
+        GPIO.output(LED_PIN, True)
+        sleep(2)
+        GPIO.output(LED_PIN, False)
+        sleep(2)
