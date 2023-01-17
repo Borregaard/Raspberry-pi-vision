dbed65a7048fdecaf5fed9e59cdb8055cc9fa62a
borregaard <frederik.borregaard@gmail.com>
2023-01-17 13:00:14 +0100

    sleep insted

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ def VideoOpencv():
 
     # Display the resulting frame
     cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        return
+    
+    sleep(5)
 
     # When everything done, release the capture
     cap.release()
