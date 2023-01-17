9d04d743508564b053892b253673e4d7f0566388
borregaard <frederik.borregaard@gmail.com>
2023-01-17 13:03:19 +0100

    only frame

--- main.py
+++ main.py
@@ -51,13 +51,13 @@ def VideoOpencv():
         ret, frame = cap.read()
 
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
         # Display the resulting frame
-        cv2.imshow('frame',gray)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             return
-    
+
         # When everything done, release the capture
         cap.release()
         cv2.destroyAllWindows()
