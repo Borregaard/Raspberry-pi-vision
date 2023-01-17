20234af09cf72b0fe51579991bdeca9e79582d4a
borregaard <frederik.borregaard@gmail.com>
2023-01-17 13:16:32 +0100

    maybe

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def VideoOpencv():
         ret, frame = cap.read()
 
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
         # Display the resulting frame
         cv2.imshow('frame', gray)
