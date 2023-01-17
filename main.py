2843280b4f1bb98a073e6bf295373ef1e378d722
borregaard <frederik.borregaard@gmail.com>
2023-01-17 13:17:31 +0100

    print message

--- main.py
+++ main.py
@@ -49,8 +49,10 @@ def VideoOpencv():
         # Capture frame-by-frame
         ret, frame = cap.read()
 
+        print(frame.shape)
+
         # Our operations on the frame come here
-        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         # Display the resulting frame
         cv2.imshow('frame', gray)
