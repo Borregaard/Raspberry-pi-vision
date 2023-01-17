f6c5e864bb8302f50d853ed619ed8b528467c4e2
borregaard <frederik.borregaard@gmail.com>
2023-01-17 13:00:59 +0100

    correct indent

--- main.py
+++ main.py
@@ -50,17 +50,17 @@ def VideoOpencv():
         # Capture frame-by-frame
         ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    sleep(5)
-
-    # When everything done, release the capture
-    cap.release()
-    cv2.destroyAllWindows()
+        # Display the resulting frame
+        cv2.imshow('frame',gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            return
+    
+        # When everything done, release the capture
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 def Laser():
