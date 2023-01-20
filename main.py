6ed8d09723830ebea8e97296b9078f6c8c0306f9
borregaard <frederik.borregaard@gmail.com>
2023-01-20 10:21:23 +0100

    pls

--- main.py
+++ main.py
@@ -7,39 +7,14 @@ import os
 
 
 LASER_PIN = 14
+def Laser():
+    GPIO.setwarnings(False)
 
-
-def CameraModule():
-    camera = PiCamera()
-    camera.resolution = (600, 300)
-    camera.start_preview()
-    # Camera warm-up time
-    sleep(2)
-    camera.capture('foo.jpg')
-
-
-def OpencvObejct():
-    file_name = 'opencv.png'
-
-    if not os.path.exists(file_name):
-        # create a PNG file
-        # in current directory
-        fp = open(file_name, 'x')
-        fp.close()
-
-    with PiCamera() as camera:
-        res = [320, 480]
-        camera.resolution = (res[0], res[1])
-        camera.start_preview()
-        camera.framerate = 24
-        sleep(2)
-        image = np.empty((res[0] * res[1] * 3,), dtype=np.uint8)
-        camera.capture(image, 'bgr')
-        image = image.reshape((res[0], res[1], 3))
-
-        status = cv2.imwrite('opencv.png', image)
-
-        print("Image written to file-system : ", status)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(LASER_PIN, GPIO.OUT)
+    GPIO.output(LASER_PIN, True)
+    sleep(5)
+    GPIO.output(LASER_PIN, False)
 
 
 class VideoCamera():
@@ -65,9 +40,8 @@ class VideoCamera():
         self.cap.release()
 
 
-
-def VideoOpencv(cap):
-
+def VideoOpencv(cap2):
+    cap = cv2.VideoCapture(0)
     frame = cap.GetFrame()
 
     # Our operations on the frame come here
@@ -82,15 +56,6 @@ def VideoOpencv(cap):
     # When everything done, release the capture
     cv2.destroyAllWindows()
 
-
-def Laser():
-    GPIO.setwarnings(False)
-
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(LASER_PIN, GPIO.OUT)
-    GPIO.output(LASER_PIN, True)
-    sleep(5)
-    GPIO.output(LASER_PIN, False)
 
 
 if __name__ == "__main__":
