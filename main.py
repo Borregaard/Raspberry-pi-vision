3137c9b69021991e1bb66d4a36b02c34d0aac6e9
borregaard <frederik.borregaard@gmail.com>
2023-01-20 11:22:19 +0100

    works code run

--- main.py
+++ main.py
@@ -59,12 +59,33 @@ def VideoOpencv(cap2):
     # When everything done, release the capture
     cv2.destroyAllWindows()
 
+def Works():
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+        cv2.imshow('gray', gray)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 if __name__ == "__main__":
+    Works()
+    
     #CaptureObejct = VideoCamera()
-    while(1):
-        
-        VideoOpencv(1)
+    #while(1):
+    #    
+    #    VideoOpencv(1)
 
     print('succuss')
